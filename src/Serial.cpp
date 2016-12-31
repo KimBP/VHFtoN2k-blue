86590ee834bb6116164c6d3db5d90cff9e526d1d
Kim Bøndergaard <kim@fam-boendergaard.dk>
2016-12-31 15:33:23 +0100

    Serial: println() methods added

--- src/Serial.cpp
+++ src/Serial.cpp
@@ -1,8 +1,25 @@
 /*
  * Serial.cpp
  *
- *  Created on: Dec 20, 2016
- *      Author: kbp
+ * 2016 Copyright (c) Kim Bøndergaard, www.fam-boendergaard.dk  All right reserved.
+ *
+ * Author: Kim Bøndergaard (kim@fam-boendergaard.dk)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this module; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-
+ *  1301  USA
+
  */
 
 #include <Serial.h>
@@ -10,12 +27,9 @@
 #include <cstdio>
 
 Serial::Serial() {
-	// TODO Auto-generated constructor stub
-
 }
 
 Serial::~Serial() {
-	// TODO Auto-generated destructor stub
 }
 
 void Serial::print(const char* str)
--- src/Serial.h
+++ src/Serial.h
@@ -2,8 +2,25 @@
 /*
  * Serial.h
  *
- *  Created on: Dec 20, 2016
- *      Author: kbp
+ * 2016 Copyright (c) Kim Bøndergaard, www.fam-boendergaard.dk  All right reserved.
+ *
+ * Author: Kim Bøndergaard (kim@fam-boendergaard.dk)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this module; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-
+ *  1301  USA
+
  */
 
 
@@ -19,7 +36,8 @@ public:
 	};
 	void print(const char* str);
 	void print(uint8_t val, SerialFormat format=DECIMAL);
-
+	void println(const char* str) { print (str); print ("\n"); };
+	void println(uint8_t val, SerialFormat format=DECIMAL) { print(val, format); print("\n"); };
 	bool available() {return true; };
 	char read();
 	bool availableForWrite() {return true;};
